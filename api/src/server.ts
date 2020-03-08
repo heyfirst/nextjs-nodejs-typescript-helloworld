527b6a4fc519798837272f57e29be1a74cfb7488
Kanisorn Sutham <firstziiz.k@gmail.com>
2020-03-08 18:48:01 +0700

    Update docker-compose

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,7 +2,7 @@ require('dotenv').config()
 import http from 'http'
 import app from './app'
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 const server = http.createServer(app)
 
